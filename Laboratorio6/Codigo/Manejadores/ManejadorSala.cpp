3ec8657c2dc8b156c96ef4bd39036c99642f059d
mike <yokadance@gmail.com>
2019-06-07 12:17:53 -0300

    creacion de manejadorSalas

---
+++ Laboratorio6/Codigo/Manejadores/ManejadorSala.cpp
@@ -0,0 +1,46 @@
+//
+// Created by mrodriguez on 07/06/19.
+//
+
+#include "ManejadorSala.h"
+#include "./Clases/Sala.h"
+#include <map>
+#include <list>
+#include <string>
+
+using namespace std;
+
+ManejadorSala* ManejadorSala::instancia = NULL;
+ManejadorSala::ManejadorSala() {}
+ManejadorSala* ManejadorSala::getInstancia() {
+    if (instancia == NULL)
+        instancia = new ManejadorSala();
+    return instancia;
+}
+
+list<Sala*> ManejadorSala::getSalas() {
+    list<Sala*> lstSalas;
+    for(map<int,Sala*>::iterator it = this->salas.begin(); it!=salas.end(); it++)
+        lstSalas.push_back(it->second);
+}
+
+void ManejadorSala::buscarSala(int x) {
+    map<int,Sala*>::iterator it = this->salas.find(x);
+    return it->second;
+}
+
+void ManejadorSala::agregarSala(Sala* sala) {
+    salas.insert(std::pair<int, Sala*>(sala->getId(), sala));
+}
+
+bool ManejadorSala::existeSala(int id){
+    map<int, Sala*>::iterator it= this->salas.find(id);
+    return (it != this-salas.end());
+}
+
+void ManejadorSala::eliminarSala(int id) {
+    map<int, Sala*>::iterator it= this->salas.find(id);
+    this->salas.erase(it);
+}
+
+ManejadorSala::~ManejadorSala(){}
