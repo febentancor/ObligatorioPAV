d62d808f7428a141f5c1c49822ea0bce5a1ec127
mike <michael@hazsoluciones.com.uy>
2019-06-10 17:46:44 -0300

    Revert "gitignore"
    This reverts commit c17018b1a46478bd988d054b34584dccba513075.

---
+++ Laboratorio6/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/DependInfo.cmake
@@ -0,0 +1,34 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/Clases/Cine.cpp" "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Clases/Cine.cpp.o"
+  "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/Clases/Funcion.cpp" "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Clases/Funcion.cpp.o"
+  "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/Clases/Pelicula.cpp" "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Clases/Pelicula.cpp.o"
+  "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/Clases/Sala.cpp" "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Clases/Sala.cpp.o"
+  "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/Dtypes/dtDireccion.cpp" "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Dtypes/dtDireccion.cpp.o"
+  "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/Dtypes/dtFecha.cpp" "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Dtypes/dtFecha.cpp.o"
+  "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/Dtypes/dtHorario.cpp" "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Dtypes/dtHorario.cpp.o"
+  "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/ManejadorUsuario.cpp" "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/ManejadorUsuario.cpp.o"
+  "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/Puntaje.cpp" "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Puntaje.cpp.o"
+  "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/Sesion.cpp" "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Sesion.cpp.o"
+  "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/Usuario.cpp" "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/Usuario.cpp.o"
+  "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/main.cpp" "/home/mrodriguez/Documentos/PAV/ObligatorioPAV/Laboratorio6/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../."
+  "../Clases"
+  "../Dtypes"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ Laboratorio6/Codigo/cmake-build-debug/CMakeFiles/Codigo.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Codigo.dir/Clases/Cine.cpp.o"
+  "CMakeFiles/Codigo.dir/Clases/Funcion.cpp.o"
+  "CMakeFiles/Codigo.dir/Clases/Pelicula.cpp.o"
+  "CMakeFiles/Codigo.dir/Clases/Sala.cpp.o"
+  "CMakeFiles/Codigo.dir/Dtypes/dtDireccion.cpp.o"
+  "CMakeFiles/Codigo.dir/Dtypes/dtFecha.cpp.o"
+  "CMakeFiles/Codigo.dir/Dtypes/dtHorario.cpp.o"
+  "CMakeFiles/Codigo.dir/main.cpp.o"
+  "CMakeFiles/Codigo.dir/ManejadorUsuario.cpp.o"
+  "CMakeFiles/Codigo.dir/Puntaje.cpp.o"
+  "CMakeFiles/Codigo.dir/Sesion.cpp.o"
+  "CMakeFiles/Codigo.dir/Usuario.cpp.o"
+  "Codigo.pdb"
+  "Codigo"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Codigo.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
